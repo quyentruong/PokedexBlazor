d1468e502e9fd6159a89dbd7ae4822d2ebdd4382
Quyen Truong <quyen.truong0202@gmail.com>
2023-03-27 01:11:41 -0700

    Add pokedex success

---
+++ PokedexBlazor/Models/PokemonLite.cs
@@ -0,0 +1,24 @@
+﻿using PokeApiNet;
+
+namespace PokedexBlazor.Models;
+
+public class PokemonLite
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public List<PokemonType> Types { get; set; }
+    public string FrontDefault { get; set; } = string.Empty;
+
+    public PokemonLite()
+    {
+        Types = new List<PokemonType>();
+    }
+
+    public PokemonLite(int id, string name, List<PokemonType> types, string frontDefault)
+    {
+        Id = id;
+        Name = name;
+        Types = types;
+        FrontDefault = frontDefault;
+    }
+}

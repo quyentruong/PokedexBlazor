b6f3d92ecceda7ccb9c066cd065e1dbf3d2fb2a4
Quyen Truong <quyen.truong0202@gmail.com>
2023-03-28 21:26:48 -0700

    implement deflat that reduce localstorage size by more than 6 times 👏

--- PokedexBlazor/Utils/BasicUtility.cs
+++ PokedexBlazor/Utils/BasicUtility.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.IO.Compression;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace PokedexBlazor.Utils;
 
@@ -56,5 +58,55 @@ public class BasicUtility
             return result;
         }
         return 0;
+    }
+
+    public static string CompressStringDeflate(string text)
+    {
+        byte[] buffer = Encoding.UTF8.GetBytes(text);
+
+        using var memoryStream = new MemoryStream();
+        using (var gzipStream = new DeflateStream(memoryStream, CompressionMode.Compress, true))
+        {
+            gzipStream.Write(buffer, 0, buffer.Length);
+        }
+
+        byte[] compressedData = memoryStream.ToArray();
+        return Convert.ToBase64String(compressedData);
+    }
+
+    public static string DecompressStringDeflate(string compressedText)
+    {
+        byte[] compressedData = Convert.FromBase64String(compressedText);
+
+        using var memoryStream = new MemoryStream(compressedData);
+        using var gzipStream = new DeflateStream(memoryStream, CompressionMode.Decompress);
+
+        using var streamReader = new StreamReader(gzipStream);
+        return streamReader.ReadToEnd();
+    }
+
+    public static string CompressStringGzip(string text)
+    {
+        byte[] buffer = Encoding.UTF8.GetBytes(text);
+        MemoryStream memoryStream = new MemoryStream();
+        using (GZipStream gzipStream = new GZipStream(memoryStream, CompressionMode.Compress, true))
+        {
+            gzipStream.Write(buffer, 0, buffer.Length);
+        }
+        memoryStream.Position = 0;
+        byte[] compressedData = new byte[memoryStream.Length];
+        memoryStream.Read(compressedData, 0, compressedData.Length);
+
+        return Convert.ToBase64String(compressedData);
+    }
+
+    public static string DecompressStringGzip(string compressedText)
+    {
+        byte[] compressedBytes = Convert.FromBase64String(compressedText);
+
+        using MemoryStream ms = new(compressedBytes);
+        using GZipStream gzip = new(ms, CompressionMode.Decompress);
+        using StreamReader reader = new(gzip);
+        return reader.ReadToEnd();
     }
 }
--- TestFunction/Program.cs
+++ TestFunction/Program.cs
@@ -1,20 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using PokeApiNet;
+using PokedexBlazor.Utils;
 
-PokeApiClient pokeClient = new PokeApiClient();
-Pokemon pikachu = await pokeClient.GetResourceAsync<Pokemon>("Mewtwo");
-//PokemonSpecies species = await pokeClient.GetResourceAsync(pikachu.Species);
-//var t = await pokeClient.GetResourceAsync(pikachu.Types[0].Type);
-var s = await pokeClient.GetResourceAsync(pikachu.Types.Select(ty => ty.Type));
+//PokeApiClient pokeClient = new PokeApiClient();
+//Pokemon pikachu = await pokeClient.GetResourceAsync<Pokemon>("Mewtwo");
+////PokemonSpecies species = await pokeClient.GetResourceAsync(pikachu.Species);
+////var t = await pokeClient.GetResourceAsync(pikachu.Types[0].Type);
+//var s = await pokeClient.GetResourceAsync(pikachu.Types.Select(ty => ty.Type));
 
-List<string> names = s.SelectMany(item => item.DamageRelations.DoubleDamageFrom)
-                      .Select(item1 => item1.Name)
-                      .ToList();
+//List<string> names = s.SelectMany(item => item.DamageRelations.DoubleDamageFrom)
+//                      .Select(item1 => item1.Name)
+//                      .ToList();
 
-foreach (var item in names)
-{
-    Console.WriteLine(item);
-}
-//Console.WriteLine(t.DamageRelations.DoubleDamageFrom[0].Name);
+//foreach (var item in names)
+//{
+//    Console.WriteLine(item);
+//}
+////Console.WriteLine(t.DamageRelations.DoubleDamageFrom[0].Name);
 
 //List<Move> allMoves = await pokeClient.GetResourceAsync(pikachu.Moves.Select(move => move.Move));

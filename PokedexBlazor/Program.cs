cde0f88544e9600e213909729c775ff20c327707
Quyen Truong <quyen.truong0202@gmail.com>
2023-04-05 02:43:57 -0700

    add animation

--- PokedexBlazor/Program.cs
+++ PokedexBlazor/Program.cs
@@ -1,10 +1,10 @@
+using BlazorAnimation;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using PokedexBlazor;
 using PokedexBlazor.Services;
-using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,6 +14,5 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddMudServices();
 builder.Services.AddScoped<PokemonService>();
-builder.Services.AddHotKeys2();
 
 await builder.Build().RunAsync();
